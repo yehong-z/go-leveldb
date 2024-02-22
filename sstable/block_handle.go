b60edebd767ffee39a0304a64612ba1657eea6dc
曾叶鸿 <1059463820@qq.com>
2024-02-22 17:11:27 +0800

    feat: sstable

---
+++ sstable/block_handle.go
@@ -0,0 +1,42 @@
+package sstable
+
+import (
+	"encoding/binary"
+	"go-leveldb/internal"
+)
+
+const (
+	kTableMagicNumber uint64 = 0xdb4775248b80fb57
+)
+
+type BlockHandle struct {
+	Offset uint32
+	Size   uint32
+}
+
+func (blockHandle *BlockHandle) EncodeToBytes() []byte {
+	p := make([]byte, 8)
+	binary.LittleEndian.PutUint32(p, blockHandle.Offset)
+	binary.LittleEndian.PutUint32(p[4:], blockHandle.Size)
+	return p
+}
+
+func (blockHandle *BlockHandle) DecodeFromBytes(p []byte) {
+	if len(p) == 8 {
+		blockHandle.Offset = binary.LittleEndian.Uint32(p)
+		blockHandle.Size = binary.LittleEndian.Uint32(p[4:])
+	}
+}
+
+type IndexBlockHandle struct {
+	*internal.InternalKey
+}
+
+func (index *IndexBlockHandle) SetBlockHandle(blockHandle BlockHandle) {
+	index.UserValue = blockHandle.EncodeToBytes()
+}
+
+func (index *IndexBlockHandle) GetBlockHandle() (blockHandle BlockHandle) {
+	blockHandle.DecodeFromBytes(index.UserValue)
+	return
+}
